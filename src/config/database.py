b658f0dd8736fc016fa93b45c0a4317158c1896b
Arturo Cazares <31503959+konakun@users.noreply.github.com>
2022-04-23 12:24:03 -0600

    Revert "Feature/ExternalConfig-Tables"

--- src/config/database.py
+++ src/config/database.py
@@ -2,14 +2,11 @@ import pymysql
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlmodel import SQLModel
-
-from src.config.config import DB_CONNECTION
 
 
-engine = create_engine(DB_CONNECTION)
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root@localhost/test_api_db'
 
-SQLModel.metadata.create_all(engine)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
